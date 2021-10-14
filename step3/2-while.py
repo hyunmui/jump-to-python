a5560213b0d21a109d03ceb81d228568662a0f8c
Hyeonmin Seon <hyunmui@outlook.kr>
2021-10-14 19:28:38 +0900

    2장 while문 마무리

---
+++ step3/2-while.py
@@ -0,0 +1,11 @@
+# while
+
+# 반복문 - simple
+treeHit = 0
+while treeHit < 10:
+    treeHit = treeHit + 1
+    print('나무를 %d번 찍었습니다.' % treeHit)
+    if treeHit == 10:
+        print('나무가 넘어갑니다')
+
+# 이외에 continue, break 을 지ㅎ원함
